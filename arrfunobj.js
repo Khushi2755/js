7c2806b765f9854beec6ae9bf638899370717858
Khushii <145796381+Khushi2755@users.noreply.github.com>
2025-01-30 09:25:13 +0000

    arrfun

---
+++ arrfunobj.js
@@ -0,0 +1,9 @@
+const arr=["go","to","hell"];
+let obj={
+    name:"prince",
+    age:18,
+}
+
+const myfun=function(){
+    console.log("hello");
+}
---
+++ opp.js
@@ -0,0 +1,26 @@
+let val=3
+let negVal=-val
+console.log(2+2);
+console.log(2**3);
+console.log(negVal);
+
+let str1="hey"
+let str2="cutiee"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log(1+2+"2");
+
+let cnt=100
+cnt++;
+console.log(cnt)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined==0)
